9d5e5f8942da09ef3bf0c372f2e07af174178d14
Joshua Marple <joshua@joshuamarple.com>
2015-04-21 17:20:02 -0500

    changed problem 1 text

--- part1/game.py
+++ part1/game.py
@@ -3,14 +3,9 @@ from pygame.locals import *
 
 """ Problems to solve:
 
-Implement a maximum velocity for the cat- make sure that it can't go too fast. 
+Make the background white
 
-Implement a "wall" feature- the cat's velocity should go to 0 whenever it hits a wall (only negating y velocity if it hits the top or bottom,
-and only negating x velocity if it hits the sides)
-
-Bonus: the cat currently negates both up and down motion when either a up or a down key is released (and same for left/right).
-Fix this so that the cat is still affected by the button currently pressed. 
-
+Make the title bar match the current time
 """
 
 def game():
